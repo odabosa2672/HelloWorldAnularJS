f8ba06b6198ce260a6edaafe16ca3a8cabcb0a9d
Odair <Odair>
2017-03-06 21:04:47 -0300

    +

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-angular.module('HelloWorldApp');
+var app = angular.module('HelloWorldApp',[]);
+
+app.controller('mainCtrl',function($scope){
+    $scope.entradaTexto = 'oda';
+    $scope.usuario = '';
+
+    $scope.exibirConteudo = function(texto){
+        alert(texto);
+    }
+
+    $scope.exibeNomeSobrenome = function(nome,sobrenome){
+        alert(nome + ' ' + sobrenome);
+    }
+});
